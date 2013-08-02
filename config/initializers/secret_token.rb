a6f4384fde62c41ec39d8b072493cd8b47518d53
David Jiang <integralhero@gmail.com>
2013-08-01 23:25:14 -0700

    Oops forgot to add files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RaagMessage::Application.config.secret_token = '272f7db3cb9514356674dad02ca36a473f6bf536dfa5569d32c3e20be2b96e565a296ed90b0bd621824b90a23f6773e4e237a88aebfc189402df48d8c0b24f45'
